e904b9375896e49d38062389589ce88522603339
minjin121277 <minjincho20@naver.com>
2025-09-12 14:22:59 +0900

    WIP: local changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,9 @@
-module.exports = {
+export default {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [],
+  addons: ['@storybook/addon-essentials'],
   framework: {
-    name: '@storybook/react',
+    name: '@storybook/react-webpack5',
     options: {},
   },
+  core: { builder: '@storybook/builder-webpack5' },
 };
